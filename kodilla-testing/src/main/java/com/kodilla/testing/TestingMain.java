0daeecda9418621501959103d0ca6e8d4d38c0ce
Roman <roman.adamczyk12@gmail.com>
2024-11-21 19:23:45 +0100

    6.2 Added Calculator class and SimpleUser class, creating the first unit test

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,35 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        System.out.println("Test - pierwszy test jednostkowy:");
+        Calculator calculator = new Calculator(5,3);
+
+        int sum = calculator.add();
+        if (sum == 8) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        int subtract = calculator.subtract();
+        if (subtract == 2) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
     }
 }
