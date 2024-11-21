f07edffb5fd79ae45aefedec7548bc811c240e78
Roman <roman.adamczyk12@gmail.com>
2024-11-21 15:10:04 +0100

    utworzenie modułu kodilla-testing

--- src/main/java/com/kodilla/records/App.java
+++ src/main/java/com/kodilla/records/App.java
@@ -2,9 +2,9 @@ package com.kodilla.records;
 
 public class App {
     public static void main(String[] args) {
-        Book book = new Book("John Novak", "Algorithms and other nightmares", 2018);
-        int year = book.getYear();
+        BookRecord book = new BookRecord("John Novak", "Algorithms and other nightmares", 2018);
         System.out.println(book);
+        int year = book.year();
         System.out.println(year);
     }
 }
