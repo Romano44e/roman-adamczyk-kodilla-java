0612eb5bfe7ab48558d01a6c2ee024e676d7390b
Roman <roman.adamczyk12@gmail.com>
2024-11-28 14:10:22 +0100

    7.2: processing submodule 7.2 and completing the task: immutable forum user.

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,11 +1,12 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.*;
 
 public class StreamMain {
     public static void main(String[] args) {
-        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
+    /*    ExpressionExecutor expressionExecutor = new ExpressionExecutor();
 
         System.out.println("Calculating expressions with lambdas");
         expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
@@ -13,11 +14,11 @@ public class StreamMain {
         expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
         expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
 
-    /*    System.out.println("Calculating expressions with method references");
+        System.out.println("Calculating expressions with method references");
         expressionExecutor.executeExpression(3,4, FunctionalCalculator::multiplyAByB);
         expressionExecutor.executeExpression(3,4, FunctionalCalculator::addAToB);
         expressionExecutor.executeExpression(3,4, FunctionalCalculator::subAFromB);
-        expressionExecutor.executeExpression(3,4, FunctionalCalculator::divideAByB); */
+        expressionExecutor.executeExpression(3,4, FunctionalCalculator::divideAByB);
 
         System.out.println();
 
@@ -25,6 +26,9 @@ public class StreamMain {
         poemBeautifier.beautify("Text example", poem -> poem.toUpperCase());
         poemBeautifier.beautify("Text example", poem -> "ABC " + poem + " ABC");
         poemBeautifier.beautify("Text example", poem -> poem.toLowerCase());
-        poemBeautifier.beautify("Text example", poem -> poem + " number 1");
+        poemBeautifier.beautify("Text example", poem -> poem + " number 1");  */
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
