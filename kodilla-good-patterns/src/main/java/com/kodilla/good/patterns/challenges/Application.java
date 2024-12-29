f9aad343ceea33e38710d2e8fef4877e4b579d94
Roman <roman.adamczyk12@gmail.com>
2024-12-29 17:37:46 +0100

    12.1 task: functional iteration over titles (additional task)

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/Application.java
@@ -0,0 +1,19 @@
+package com.kodilla.good.patterns.challenges;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+        MovieStore movieStore = new MovieStore();
+        String collect = movieStore.getMovies().entrySet().stream()
+                .flatMap(entry -> entry.getValue().stream())
+                .collect(Collectors.joining("!"));
+        
+        System.out.println(collect);
+    }
+}
